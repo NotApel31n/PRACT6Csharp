b747c7de9baed3be12430c49be8bd945bae837d1
timba <timba@DESKTOP-QU2GJFR>
2022-12-26 20:43:51 +0300

    Добавьте файлы проекта.

---
+++ Snake/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp19
+{
+    public class Figure
+    {
+        public Figure() { }
+        public Figure(string _name, int _width, int _height) { name = _name; width = _width; height = _height; }
+        public string getStringed()
+        {
+            return $"{name}\n{width}\n{height}";
+        }
+        public string name;
+        public int width;
+        public int height;
+    }
+}
